775fbf432fe10bd1ab2350a2bc5e40ae119fe909
Luis Saiete <luistsaiete@gmail.com>
2021-12-20 09:41:18 +0200

    Mostrando o resumo das transacoes

--- scripts.js
+++ scripts.js
@@ -30,9 +30,41 @@ const transactions = [
 ];
 
 const Transaction = {
-  incomes() {},
-  expenses() {},
-  total() {},
+  all: transactions,
+  add(transaction) {
+    Transaction.all.push(transaction);
+  },
+  incomes() {
+    let income = 0;
+
+    Transaction.all.forEach((transaction) => {
+      if (transaction.amount > 0) {
+        income += transaction.amount;
+      }
+    });
+
+    return income;
+  },
+  expenses() {
+    let expense = 0;
+
+    Transaction.all.forEach((transaction) => {
+      if (transaction.amount < 0) {
+        expense -= transaction.amount;
+      }
+    });
+
+    return expense;
+  },
+  total() {
+    let total = 0;
+
+    Transaction.all.forEach((transaction) => {
+      total += transaction.amount;
+    });
+
+    return total;
+  },
 };
 
 const DOM = {
@@ -49,7 +81,7 @@ const DOM = {
     const amount = Utils.formatCurrency(transaction.amount);
     const html = `
 			<td class="description">${transaction.description}</td>
-			<td class="${amountClass}">${transaction.amount}</td>
+			<td class="${amountClass}">${amount}</td>
 			<td class="date">${transaction.date}</td>
 
 			<td><img src="./assets/minus.svg" alt="Remover Transação" /></td>
@@ -58,14 +90,37 @@ const DOM = {
 
     return html;
   },
+  updateBalance() {
+    document.getElementById("income-display").innerHTML = Utils.formatCurrency(
+      Transaction.incomes()
+    );
+    document.getElementById("expense-display").innerHTML = Utils.formatCurrency(
+      Transaction.expenses()
+    );
+    document.getElementById("total-display").innerHTML = Utils.formatCurrency(
+      Transaction.total()
+    );
+  },
 };
 
 const Utils = {
   formatCurrency(value) {
     const signal = value < 0 ? "-" : "";
+
+    value = String(value).replace(/\D/g, "");
+
+    value = Number(value) / 100;
+
+    value = value.toLocaleString("pt-mz", {
+      style: "currency",
+      currency: "MZN",
+    });
+    return signal + value;
   },
 };
 
 transactions.forEach((transaction) => {
   DOM.addTransaction(transaction);
 });
+
+DOM.updateBalance();
