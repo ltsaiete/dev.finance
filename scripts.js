8b93c8e51ad8791d3653791b49c649afc28352f9
Luis Saiete <luistsaiete@gmail.com>
2021-12-21 10:43:17 +0200

    Validando os dados do form

--- scripts.js
+++ scripts.js
@@ -111,12 +111,66 @@ const DOM = {
 };
 
 const Form = {
+  description: document.querySelector("input#description"),
+  amount: document.querySelector("input#amount"),
+  date: document.querySelector("input#date"),
+  getValues() {
+    return {
+      description: Form.description.value,
+      amount: Form.amount.value,
+      date: Form.date.value,
+    };
+  },
+  validateFields() {
+    const { description, amount, date } = Form.getValues();
+
+    if (
+      description.trim() === "" ||
+      amount.trim() === "" ||
+      date.trim() === ""
+    ) {
+      throw new Error("Por favor, preencha todos os campos");
+    }
+  },
+
+  formatValues() {
+    let { description, amount, date } = Form.getValues();
+    amount = Utils.formatAmount(amount);
+
+    date = Utils.formatDate(date);
+
+    return {
+      description,
+      amount,
+      date,
+    };
+  },
+
   submit(event) {
-    console.log(event);
+    event.preventDefault();
+
+    try {
+      Form.validateFields();
+
+      const transaction = Form.formatValues();
+    } catch (error) {
+      alert(error.message);
+    }
   },
 };
 
 const Utils = {
+  formatAmount(value) {
+    value = Number(value) * 100;
+
+    return value;
+  },
+
+  formatDate(date) {
+    const [year, month, day] = date.split("-");
+
+    return `${day}/${month}/${year}`;
+  },
   formatCurrency(value) {
     const signal = value < 0 ? "-" : "";
 
