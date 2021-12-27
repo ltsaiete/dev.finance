e4c47c2058c845a3209ecef6b0ab3b2935b5924f
Luis Saiete <luistsaiete@gmail.com>
2021-12-27 05:56:05 +0200

    FIxing amount format

--- scripts.js
+++ scripts.js
@@ -172,9 +172,9 @@ const Form = {
 
 const Utils = {
   formatAmount(value) {
-    value = Number(value) * 100;
+    value = value * 100;
 
-    return value;
+    return Math.round(value);
   },
 
   formatDate(date) {
